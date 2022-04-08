dca53eae504da98e83db9377bb8d671259bd1c54
nedimfejzic <fejzic.nedim@outlook.com>
2022-04-08 23:07:48 +0200

    sound card and timer

---
+++ Client/wwwroot/scripts/player.js
@@ -0,0 +1,16 @@
+﻿function SetPlayerVolume(playerId, newVolume) {
+    document.getElementById(playerId).volume = newVolume;
+    console.log("playerId - " + playerId + "| volume is - "+ newVolume);
+}
+
+
+function StartPlayer(playerId) {
+
+    document.getElementById(playerId).play()
+}
+
+
+function StopPlayer(playerId) {
+
+    document.getElementById(playerId).pause()
+}
